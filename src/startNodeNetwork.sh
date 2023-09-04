02658cc5510c725da53ec09906b7142424e2cf6c
Emil Nyberg <emilnyberg1998@gmail.com>
2023-09-04 11:10:43 +0200

    Removed wait command in bash script

--- src/startNodeNetwork.sh
+++ src/startNodeNetwork.sh
@@ -3,15 +3,8 @@
 #Build the docker image and give it the correct name
 sudo docker build . -t kadlab
 
-echo "wait 30 seconds for the image to build"
-sudo wait 30
-
 #Start the docker containers
 sudo docker-compose up -d
-
-#Wait for the containers to start up
-echo "wait 5 seconds for the containers to start up"
-sudo wait 5
 
 #Check which containers are running
 sudo docker-compose ps
