c5ec8d8168c1a262edbc295ccea82cbdcb9435d6
Emil Nyberg <emilnyberg1998@gmail.com>
2023-09-04 11:45:41 +0200

    fixed bash script

--- src/startNodeNetwork.sh
+++ src/startNodeNetwork.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-export COMPOSE_HTTP_TIMEOUT=200
-
 #Build the docker image and give it the correct name
 sudo docker build . -t kadlab
 
 #Start the docker containers
-sudo docker-compose up -d
+sudo COMPOSE_HTTP_TIMEOUT=2500 docker-compose up
 
 #Check which containers are running
 sudo docker-compose ps
