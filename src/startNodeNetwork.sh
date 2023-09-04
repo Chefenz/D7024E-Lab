92bc6efaaa86314891c79a41e882aac24f38a498
Emil Nyberg <emilnyberg1998@gmail.com>
2023-09-04 13:24:18 +0200

    fixed git bash file

--- src/startNodeNetwork.sh
+++ src/startNodeNetwork.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#Remove any running containers
+echo "Remove any running containers"
+sudo docker-compose down
+
 #Build the docker image and give it the correct name
+echo "Build the docker file"
 sudo docker build . -t kadlab
 
 #Start the docker containers
-sudo COMPOSE_HTTP_TIMEOUT=2500 docker-compose up
+echo "start the docker containers"
+sudo COMPOSE_HTTP_TIMEOUT=2500 docker-compose up -d
 
 #Check which containers are running
-sudo docker-compose ps
+sudo docker ps
