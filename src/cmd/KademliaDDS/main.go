39b58ef7badea5d9aaa4578a2006d822a0901a29
Emil Nyberg <emilnyberg1998@gmail.com>
2023-09-14 10:46:54 +0200

    The docker files should now be completely done. Did some cleanup and merged the script that compiled the go program with the script that fires up the docker containers

--- src/cmd/KademliaDDS/main.go
+++ src/cmd/KademliaDDS/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	//Use KEY = 'HOSTNAME' aswell later to retrive the address of the node
 	containerName := os.Getenv("CONTAINER_NAME")
 	fmt.Printf("Container Name: %s\n", containerName)
 }
