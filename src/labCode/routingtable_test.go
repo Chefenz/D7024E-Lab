5da8db6a37eca177e9acedcf0299bf389063ae61
Pontus Schünemann <pingispontus@gmail.com>
2023-09-21 17:39:27 +0200

    Added more tests to routingtable_test

--- src/labCode/routingtable_test.go
+++ src/labCode/routingtable_test.go
@@ -1,11 +1,33 @@
 package labCode
 
-/*
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+func TestNewRoutingTable(t *testing.T) {
+	testContact1 := NewContact(NewKademliaID("TEST1"), "TEST_ADDRES1")
+
+	bucketChan := make(chan Contact, 1)
+	bucketWaitChan := make(chan bool)
+	findChan := make(chan Contact)
+	returnFindChan := make(chan []Contact)
+	routingTable := NewRoutingTable(testContact1, &bucketChan, &bucketWaitChan, &findChan, &returnFindChan)
+
+	var testBuckets [IDLength * 8]*bucket
+	for i := 0; i < IDLength*8; i++ {
+		testBuckets[i] = newBucket()
+	}
+
+	routingTable.buckets = testBuckets
+
+	testRoutingTable := RoutingTable{Me: testContact1, buckets: testBuckets, BucketChan: &bucketChan, BucketWaitChan: &bucketWaitChan, FindChan: &findChan, ReturnFindChan: &returnFindChan}
+	testTable := &testRoutingTable
+	assert.Equal(t, routingTable, testTable)
+}
+
+/*
 func TestRoutingTable(t *testing.T) {
 	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
 
